3f780f5b9821d25b3c8aac077b9fb53a833fc899
mejiamagna <51210347+mejiamagna@users.noreply.github.com>
2023-04-13 09:04:06 -0500

    JS

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
-//console.log('Hello!');
+console.log('Ups Ecuador!');
 
-let bubbles = document.querySelector('.bubbles');
-let bubbleCount = 60;
-let i = 0;
+function addLink() {
+  console.log('add link');
+  var link = document.createElement('li');
+  link.innerHTML = "<a href='https://www.google.com/'>google</a>";
+  var ul = document.querySelector('aside ul');
+  ul.appendChild(link);
 
-while (i < bubbleCount) {
-  let bubble = document.createElement('div');
-  let size = Math.floor(Math.random() * 200) + 100;
-  let top = Math.floor(Math.random() * window.innerHeight);
-  let left = Math.floor(Math.random() * window.innerWidth);
+  var list3 = document.querySelectorAll('aside li:nth-child(odd)');
+  for (let i of list3) {
+    list3.classList.add('desplazar');
+  }
 
-  bubble.style.width = size + 'px';
-  bubble.style.height = size + 'px';
-  bubble.style.top = top + 'px';
-  bubble.style.left = left + 'px';
-  bubbles.appendChild(bubble);
+  setTimeout(function () {
+    for (let li of list3) {
+      li.classList.remove('desplazar');
+    }
+  }, 1000);
+}
 
-  i++;
+var i = 0;
+
+var elemento = document.getElementById('titulo1');
+elemento.addEventListener('click', function () {
+  //alert('Click en titulo');
+  //elemento.innerHTML = i
+  //crearParrafo()
+});
+
+elemento.addEventListener('mouseover', function () {
+  i = i + 1;
+  console.log(i);
+});
+
+//var menu = document.getElementById('menu');
+//var listas = menu.getElementsByTagName("li")
+//console.log(listas)
+
+//var list = document.querySelectorAll("#menu li")
+//console.log(list)
+
+var list2 = document.querySelectorAll('aside li');
+console.log(list2);
+
+var list3 = document.querySelectorAll('aside li:nth-child(odd)');
+console.log(list3);
+
+function crearParrafo() {
+  // Crea un elemento <p> y le añade texto
+  var miParrafo = document.createElement('p');
+  var texto = document.createTextNode(
+    'Este es un ejemplo de cómo crear un elemento en HTML con JavaScript'
+  );
+  miParrafo.appendChild(texto);
+
+  // Añade el elemento <p> creado al div con el id "miDiv"
+  document.body.appendChild(miParrafo);
 }
